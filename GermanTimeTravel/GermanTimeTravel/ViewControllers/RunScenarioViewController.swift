97ba1260b7cf0deffe89bfca86723b9f1dced8eb
Zachary Thacker <zachthac@aol.com>
2021-01-15 19:34:56 -0500

    Merged and added UI

---
+++ GermanTimeTravel/GermanTimeTravel/ViewControllers/RunScenarioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RunScenarioViewController.swift
+//  GermanTimeTravel
+//
+//  Created by Zachary Thacker on 1/15/21.
+//
+
+import UIKit
+
+class RunScenarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
