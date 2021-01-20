729dd7ee271f198cb6878c8ef99559845e2a7ac6
Zachary Thacker <zachthac@aol.com>
2021-01-20 15:39:46 -0500

    Added gradient layers

--- GermanTimeTravel/GermanTimeTravel/ViewControllers/AllScenariosViewController.swift
+++ GermanTimeTravel/GermanTimeTravel/ViewControllers/AllScenariosViewController.swift
@@ -13,7 +13,10 @@ class AllScenariosViewController: UIViewController {
     
     // MARK: - Outlets
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleView: UIView!
     @IBOutlet private var tableView: UITableView!
+    @IBOutlet weak var roundView: UIView!
     
     // MARK: - Properties
     
@@ -24,8 +27,47 @@ class AllScenariosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        setUpViews()
     }
+    
+    private func setUpViews() {
+        gradient1.frame = roundView.bounds
+        gradient2.frame = titleView.bounds
+        titleView.layer.addSublayer(gradient2)
+        titleView.bringSubviewToFront(titleLabel)
+        roundView.layer.addSublayer(gradient1)
+        roundView.roundCorners(cornerRadius: 25)
+        tableView.roundCorners(cornerRadius: 25)
+        
+        
+        
 
+        
+    }
+    
+    lazy var gradient1: CAGradientLayer = {
+        let gradient1 = CAGradientLayer()
+        gradient1.type = .axial
+        gradient1.colors = [
+            UIColor(named: "LightBlue")?.cgColor,
+            UIColor(named: "DarkBlue")?.cgColor
+        ]
+        gradient1.locations = [0, 1]
+        return gradient1
+    }()
+    lazy var gradient2: CAGradientLayer = {
+        let gradient2 = CAGradientLayer()
+        gradient2.type = .radial
+        gradient2.colors = [
+            UIColor(named: "LightBlue")?.cgColor,
+            UIColor(named: "DarkBlue")?.cgColor
+        ]
+        gradient2.startPoint = CGPoint(x: 0.5, y: 0.75)
+        let endY = 1 + view.frame.size.width / view.frame.size.height
+        gradient2.endPoint = CGPoint(x: 1.1, y: endY)
+            return gradient2
+    }()
+    
 }
 
 extension AllScenariosViewController: UITableViewDataSource {
@@ -36,6 +78,7 @@ extension AllScenariosViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "scenarioCell", for: indexPath) as! ScenarioTableViewCell
         cell.scenario = controller?.summaries?[indexPath.row]
+        cell.layer.backgroundColor = UIColor.clear.cgColor
         return cell
     }
 }
--- GermanTimeTravel/GermanTimeTravel/ViewControllers/HomeViewController.swift
+++ GermanTimeTravel/GermanTimeTravel/ViewControllers/HomeViewController.swift
@@ -11,11 +11,52 @@ class HomeViewController: UIViewController {
     
     let controller = ModelController()
     
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var roundView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         controller.setPreferences()
         controller.signInAndGetScenarioList()
+        setUpViews()
     }
+    
+    private func setUpViews() {
+        gradient1.frame = roundView.bounds
+        gradient2.frame = titleView.bounds
+        roundView.layer.addSublayer(gradient1)
+        titleView.layer.addSublayer(gradient2)
+        titleView.bringSubviewToFront(titleLabel)
+        roundView.roundCorners(cornerRadius: 25)
+        roundView.bringSubviewToFront(stackView)
+        
+        
+    }
+    
+    lazy var gradient1: CAGradientLayer = {
+        let gradient1 = CAGradientLayer()
+        gradient1.type = .axial
+        gradient1.colors = [
+            UIColor(named: "LightBlue")?.cgColor,
+            UIColor(named: "DarkBlue")?.cgColor
+        ]
+        gradient1.locations = [0, 1]
+        return gradient1
+    }()
+    lazy var gradient2: CAGradientLayer = {
+        let gradient2 = CAGradientLayer()
+        gradient2.type = .radial
+        gradient2.colors = [
+            UIColor(named: "LightBlue")?.cgColor,
+            UIColor(named: "DarkBlue")?.cgColor
+        ]
+        gradient2.startPoint = CGPoint(x: 0.5, y: 0.75)
+        let endY = 1 + view.frame.size.width / view.frame.size.height
+        gradient2.endPoint = CGPoint(x: 1.1, y: endY)
+            return gradient2
+    }()
   
     // MARK: - Navigation
 
