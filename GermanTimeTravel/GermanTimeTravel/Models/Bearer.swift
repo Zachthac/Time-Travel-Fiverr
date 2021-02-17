6dac4555338d80f0da85efa4a45e23868ff454dd
Cora Jacobson <cora-jacobson@lambdastudents.com>
2021-02-17 11:12:39 -0800

    Added date to Bearer to check token expiration

--- GermanTimeTravel/GermanTimeTravel/Models/Bearer.swift
+++ GermanTimeTravel/GermanTimeTravel/Models/Bearer.swift
@@ -10,4 +10,17 @@ import Foundation
 struct Bearer: Codable {
     var access_token: String
     var token_type: String
+    var date: Date
+    
+    enum Keys: String, CodingKey {
+        case access_token
+        case token_type
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        access_token = try container.decode(String.self, forKey: .access_token)
+        token_type = try container.decode(String.self, forKey: .token_type)
+        date = Date()
+    }
 }
